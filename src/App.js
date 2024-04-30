7ac025cfb3f78346783c0080ff8146d483d4de8c
samipthakkar <samipthakkarr44@gmail.com>
2024-04-29 23:34:21 -0700

    Inital setup, basic call to external API and imported styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import searchIcon from './search.svg';
 
-function App() {
-  return (
+const API_URL = 'http://www.omdbapi.com?apikey=62c76933';
+
+const App = () => {
+
+  const searchMovies = async (title) => {
+    const response =  await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json()
+    console.log(data.Search)
+  }
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  },[]);
+
+  return (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie Land</h1>
     </div>
   );
 }
